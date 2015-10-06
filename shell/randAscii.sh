6e20f894cf4a50ff56225bf789e9a289de75cca3
Nik Krimm <nkrimm@signal.co>
2015-10-06 09:13:48 -0500

    more wonderfulness

---
+++ shell/randAscii.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+case $[($RANDOM % 11)  + 1] in
+  1)
+    ack --bar 
+    ;;
+  2)
+    ack --cathy
+    ;;
+  3)
+    ack --thppt
+    ;;
+  4)
+    cat ~/dotfiles/ascii/code_clean
+    ;;
+  5)
+    cat ~/dotfiles/ascii/dragon1
+    ;;
+  6)
+    cat ~/dotfiles/ascii/dragon2
+    ;;
+  7)
+    cat ~/dotfiles/ascii/dragon3
+    ;;
+  8)
+    cat ~/dotfiles/ascii/unicorn
+    ;;
+  9)
+    cat ~/dotfiles/ascii/atom
+    ;;
+  10)
+    cat ~/dotfiles/ascii/cakelie
+    ;;
+  11)
+    cat ~/dotfiles/ascii/honeybee
+    ;;
+  12) 
+    cat ~/dotfiles/ascii/kill
+    ;;
+  13)
+    cat ~/dotfiles/ascii/unicorn2
+    ;;
+
+esac
+
+
