e65ef5d277b259ba7df2dd8f9ebe679c45c8d237
Nik Krimm <nkrimm@signal.co>
2016-03-08 22:57:00 -0600

    node random ascii

---
+++ shell/randAscii.js
@@ -0,0 +1,16 @@
+var fs = require('fs'),
+  path = require('path');
+
+function randomFrom(list) {
+  return list[Math.floor(Math.random() *list.length)];
+}
+
+function write(err, contents) {
+  console.log(contents.toString());
+}
+
+function catRandomFile(err, list) {
+  fs.readFile(path.join(__dirname, '../ascii', randomFrom(list)), write);
+}
+
+fs.readdir(path.join(__dirname, '../ascii'), catRandomFile);
--- shell/randAscii.sh
+++ shell/randAscii.sh
@@ -1,51 +1,7 @@
 #!/bin/bash
 
-case $[($RANDOM % 15)  + 1] in
-  1)
-    ack --bar 
-    ;;
-  2)
-    ack --cathy
-    ;;
-  3)
-    ack --thppt
-    ;;
-  4)
-    cat ~/dotfiles/ascii/code_clean
-    ;;
-  5)
-    cat ~/dotfiles/ascii/dragon1
-    ;;
-  6)
-    cat ~/dotfiles/ascii/dragon2
-    ;;
-  7)
-    cat ~/dotfiles/ascii/dragon3
-    ;;
-  8)
-    cat ~/dotfiles/ascii/unicorn
-    ;;
-  9)
-    cat ~/dotfiles/ascii/atom
-    ;;
-  10)
-    cat ~/dotfiles/ascii/cakelie
-    ;;
-  11)
-    cat ~/dotfiles/ascii/honeybee
-    ;;
-  12) 
-    cat ~/dotfiles/ascii/kill
-    ;;
-  13)
-    cat ~/dotfiles/ascii/unicorn2
-    ;;
-  14) 
-    cat ~/dotfiles/ascii/yoda
-    ;;
-  15) 
-    cat ~/dotfiles/ascii/nemisys
-
-esac
-
-
+fs.readdir('../ascii', function (err, list) { 
+  fs.readFile('../ascii/' + list[Math.floor(Math.random() *list.length)], function (err, contents) { 
+    console.log(contents.toString()) 
+  }) 
+})
