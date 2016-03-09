60f179fdbce4ef18671e6750bb780770ec83e044
Nik Krimm <nkrimm@signal.co>
2016-03-08 22:30:40 -0600

    add some more cool ascii

--- shell/randAscii.sh
+++ shell/randAscii.sh
@@ -1,7 +1,46 @@
 #!/bin/bash
 
-fs.readdir('../ascii', function (err, list) { 
-  fs.readFile('../ascii/' + list[Math.floor(Math.random() *list.length)], function (err, contents) { 
-    console.log(contents.toString()) 
-  }) 
-})
+case $[($RANDOM % 11)  + 1] in
+  1)
+    cat ~/dotfiles/ascii/killbugs
+    ;;
+  2)
+    ack --bar 
+    ;;
+  3)
+    cat ~/dotfiles/ascii/syndicate
+    ;;
+  4)
+    cat ~/dotfiles/ascii/code_clean
+    ;;
+  5)
+    cat ~/dotfiles/ascii/dragon1
+    ;;
+  6)
+    cat ~/dotfiles/ascii/dragon2
+    ;;
+  7)
+    cat ~/dotfiles/ascii/dragon3
+    ;;
+  8)
+    cat ~/dotfiles/ascii/unicorn
+    ;;
+  9)
+    cat ~/dotfiles/ascii/atom
+    ;;
+  10)
+    cat ~/dotfiles/ascii/cakelie
+    ;;
+  11)
+    cat ~/dotfiles/ascii/honeybee
+    ;;
+  12) 
+    cat ~/dotfiles/ascii/kill
+    ;;
+  13)
+    cat ~/dotfiles/ascii/unicorn2
+    ;;
+
+esac
+
+
