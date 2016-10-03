afc656f3d9c4148863bfbb1b1466a69d90bdd238
Nik Krimm <nkrimm@signal.co>
2016-10-03 10:43:42 -0500

    adding rebase_and_merge function

--- git/git_helpers.bash
+++ git/git_helpers.bash
@@ -52,3 +52,7 @@ postgreview ()
 }
 
 
+function rebase_and_merge {
+  BRANCH=$(parse_git_branch)
+  gbout master && git co master && git pull origin master && git co $BRANCH && git rebase master && git co master &&  git merge $BRANCH
+}
